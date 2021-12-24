f37f13595151dd3a800102c0abbb1d632726142d
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2021-12-23 23:00:43 -0300

    Implementing team role creation

--- opsgenie/resource_opsgenie_team_role.go
+++ opsgenie/resource_opsgenie_team_role.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/opsgenie/opsgenie-go-sdk-v2/team"
 )
 
 func resourceOpsGenieTeamRole() *schema.Resource {
@@ -25,30 +25,82 @@ func resourceOpsGenieTeamRole() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"granted_rights": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Schema{
-					Type:         schema.TypeString,
-					ValidateFunc: validation.StringInSlice(validCustomRolesRights, false),
+			"rights": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"right": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"granted": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  true,
+						},
+					},
 				},
-				Set: schema.HashString,
-			},
-			"disallowed_rights": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Schema{
-					Type:         schema.TypeString,
-					ValidateFunc: validation.StringInSlice(validCustomRolesRights, false),
-				},
-				Set: schema.HashString,
 			},
 		},
 	}
 }
 
+// TODO: Read and set values before returning
 func resourceOpsGenieTeamRoleCreate(d *schema.ResourceData, meta interface{}) error {
+	client, err := team.NewClient(meta.(*OpsgenieClient).client.Config)
+	if err != nil {
+		return err
+	}
+
+	name := d.Get("name").(string)
+	teamId := d.Get("team_id").(string)
+
+	createRequest := team.CreateTeamRoleRequest{
+		TeamIdentifierType:  team.Id,
+		TeamIdentifierValue: teamId,
+		Name:                name,
+	}
+	createRequest.Rights = expandOpsGenieTeamRoleRights(d)
+
+	_, err = client.CreateRole(context.Background(), &createRequest)
+	if err != nil {
+		return err
+	}
+
+	getRequest := &team.GetTeamRoleRequest{
+		TeamID:   teamId,
+		RoleName: name,
+	}
+
+	getResponse, err := client.GetRole(context.Background(), getRequest)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(getResponse.Id)
 	return nil
+}
+
+func expandOpsGenieTeamRoleRights(d *schema.ResourceData) []team.Right {
+	input := d.Get("rights").([]interface{})
+	rights := make([]team.Right, 0, len(input))
+
+	for _, v := range input {
+		config := v.(map[string]interface{})
+
+		rightName := config["right"].(string)
+		granted := config["granted"].(bool)
+
+		right := team.Right{
+			Right:   rightName,
+			Granted: &granted,
+		}
+
+		rights = append(rights, right)
+	}
+
+	return rights
 }
 
 func resourceOpsGenieReadContext(context.Context, *schema.ResourceData, interface{}) diag.Diagnostics {
